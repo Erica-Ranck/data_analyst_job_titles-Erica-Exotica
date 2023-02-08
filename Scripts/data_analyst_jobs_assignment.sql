a452ac33d85258651ab8a95fe1a4d3d179d08502
Erica Ranck <ericaranck1@gmail.com>
2023-02-07 19:17:14 -0600

    7 push

--- Scripts/data_analyst_jobs_assignment.sql
+++ Scripts/data_analyst_jobs_assignment.sql
@@ -57,6 +57,11 @@ ORDER BY avg_rating desc;
 
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
+
+-- there are 881 distinct job titles.
+
 -- 8.	How many unique job titles are there for California companies?
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
