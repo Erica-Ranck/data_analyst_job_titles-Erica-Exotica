e599243daf8d3776565b8bc208a15706e61a56fa
Erica Ranck <ericaranck1@gmail.com>
2023-02-07 19:38:26 -0600

    11 push

--- Scripts/data_analyst_jobs_assignment.sql
+++ Scripts/data_analyst_jobs_assignment.sql
@@ -91,6 +91,10 @@ ORDER BY avg_rating DESC;
 
 -- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
 
+SELECT COUNT(title)
+FROM data_analyst_jobs
+WHERE title LIKE '%Analyst%';
+
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
 -- **BONUS:**
