7918f451b0f7fd29c39398c68923beb9e136d361
Erica Ranck <ericaranck1@gmail.com>
2023-02-07 18:59:46 -0600

    second push

--- Scripts/data_analyst_jobs_assignment.sql
+++ Scripts/data_analyst_jobs_assignment.sql
@@ -11,6 +11,12 @@ FROM data_analyst_jobs;
 
 -- 2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row?
 
+SELECT *
+FROM data_analyst_jobs
+LIMIT 10;
+
+--ExxonMobil
+
 -- 3.	How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
 
 -- 4.	How many postings in Tennessee have a star rating above 4?
